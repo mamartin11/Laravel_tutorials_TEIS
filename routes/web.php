df113e3a158265ac728a874b2fa6df3824e2a758
mamartin11 <mamartin11@eafit.edu.co>
2025-02-17 17:40:43 -0500

    Activity 2

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/about', function () {
+
+    $data1 = "About us - Online Store";
+    
+    $data2 = "About us";
+    
+    $description = "This is an about page ...";
+    
+    $author = "Developed by: Miguel Martínez";
+    
+    return view('home.about')->with("title", $data1)
+    
+    ->with("subtitle", $data2)
+    
+    ->with("description", $description)
+    
+    ->with("author", $author);
+    
+    })->name("home.about");
+
+
+Route::get('/contact', function () {
+
+    $data1 = "Contact us - Online Store";
+    
+    $data2 = "Contact us";
+    
+    $email = "fakecompany@gmail.com";
+    
+    $address = "441 N Fairfax Ave, Los Angeles, CA 90036";
+
+    $phone_number = "+1 323-424-4504";
+    
+    return view('home.about')->with("title", $data1)
+    
+    ->with("subtitle", $data2)
+    
+    ->with("description", $email)
+    
+    ->with("author", $address)
+
+    ->with("author", $phone_number);
+    
+    })->name("home.contact");
